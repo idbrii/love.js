dc8fdfddbf31ec50745831f46afc2e4b27954177
Ruin0x11 <ipickering2@gmail.com>
2020-02-05 21:57:36 -0800

    add megasource
    Cherry-picked from Ruin0x11's original commit
    https://github.com/Ruin0x11/love.js/commit/ea01289f5c0f9c9715dadeb503a1407dac1b7bfb
    
    Modified to use love's megasource instead.

--- build_lovejs.sh
+++ build_lovejs.sh
@@ -4,20 +4,24 @@ cd "$(dirname "$0")"
 
 mkdir -p build/debug build/release
 
-source ~/git/emsdk/emsdk_env.sh
+source ./emsdk/emsdk_env.sh
+
+## WASM2JS in emsdk does not support pthreads. Use asm.js instead.
+#export EMCC_WASM_BACKEND=0
 
 # (
 #   cd build/debug
-#   emcmake cmake /Users/dkhachat/git/megasource -DLOVE_JIT=0 -DCMAKE_BUILD_TYPE=Debug
+#   emcmake cmake ../../megasource -DLOVE_JIT=0 -DCMAKE_BUILD_TYPE=Debug
 #   emmake make -j 6
-#   cp love/love.js* ../../src/debug
-#   cp love/love.wasm ../../src/debug
-#   cp love/love.worker.js ../../src/debug
+#   # cp love/love.js* ../../src/debug
+#   # cp love/love.wasm ../../src/debug
+#   # cp love/love.worker.js ../../src/debug
+#   # cp love/pthread-main.js ../../src/debug
 # )
 
 (
   cd build/release
-  emcmake cmake /Users/dkhachat/git/megasource -DLOVE_JIT=0 -DCMAKE_BUILD_TYPE=Release
+  emcmake cmake ../../megasource -DLOVE_JIT=0 -DCMAKE_BUILD_TYPE=Release
   emmake make -j 6
   cp love/love.js* ../../src/release
   cp love/love.wasm ../../src/release
